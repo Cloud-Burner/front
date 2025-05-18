5669b333a9ecd18acabc90bb59292d34640a2448
marinaslobozaninova <slobozhaninovaw@gmail.com>
2025-05-18 17:04:26 +0300

    fix: timer

--- src/pages/TerminalVideoPage.tsx
+++ src/pages/TerminalVideoPage.tsx
@@ -63,7 +63,7 @@ const StreamWithTerminal: React.FC = () => {
     }, []);
 
     const formatCountdownTime = (seconds: number) => {
-        const hours = Math.floor(seconds / 3600) + 2;
+        const hours = Math.floor(seconds / 3600) ;
         const minutes = Math.floor((seconds % 3600) / 60);
         return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`;
     };
